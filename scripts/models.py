093a6fc7feca239ba6786efae7e2bdefae115f21
Lennart Muehlhahn <lennart.muehlhahn@tuhh.de>
2023-11-06 17:04:29 +0100

    Changed models.py, to include validation_data

--- scripts/models.py
+++ scripts/models.py
@@ -19,11 +19,11 @@ if __name__ == '__main__':
     dnn1_model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
     print("DNN, 1 Hidden Layer, 5 Epochs")
-    dnn1_model.fit(train_images, train_labels, epochs=5)
+    dnn1_model.fit(train_images, train_labels, epochs=5, validation_data=(test_images, test_labels))
     keras.saving.save_model(dnn1_model, "../models/dnn_hid1_gelu_5epoch")
 
     print("DNN, 1 Hidden Layer, 10 Epochs")
-    dnn1_model.fit(train_images, train_labels, epochs=5)
+    dnn1_model.fit(train_images, train_labels, epochs=5, validation_data=(test_images, test_labels))
     keras.saving.save_model(dnn1_model, "../models/dnn_hid1_gelu_10epoch")
 
     dnn2_model = keras.Sequential([
@@ -36,11 +36,11 @@ if __name__ == '__main__':
     dnn2_model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
     print("DNN, 2 Hidden Layers, 5 Epochs")
-    dnn2_model.fit(train_images, train_labels, epochs=5)
+    dnn2_model.fit(train_images, train_labels, epochs=5, validation_data=(test_images, test_labels))
     keras.saving.save_model(dnn2_model, "../models/dnn_hid2_gelu_5epoch")
 
     print("DNN, 2 Hidden Layers, 10 Epochs")
-    dnn2_model.fit(train_images, train_labels, epochs=5)
+    dnn2_model.fit(train_images, train_labels, epochs=5, validation_data=(test_images, test_labels))
     keras.saving.save_model(dnn2_model, "../models/dnn_hid2_gelu_10epoch")
 
     cnn1_model = keras.Sequential([
@@ -54,11 +54,11 @@ if __name__ == '__main__':
     cnn1_model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
     print("CNN, 1 Convolution Layer, 5 Epochs")
-    cnn1_model.fit(train_images, train_labels, epochs=5)
+    cnn1_model.fit(train_images, train_labels, epochs=5, validation_data=(test_images, test_labels))
     keras.saving.save_model(cnn1_model, "../models/cnn_conv1_gelu_5epoch")
 
     print("CNN, 1 Convolution Layer, 10 Epochs")
-    cnn1_model.fit(train_images, train_labels, epochs=5)
+    cnn1_model.fit(train_images, train_labels, epochs=5, validation_data=(test_images, test_labels))
     keras.saving.save_model(cnn1_model, "../models/cnn_conv1_gelu_10epoch")
     
     cnn2_model = keras.Sequential([
@@ -74,9 +74,9 @@ if __name__ == '__main__':
     cnn2_model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
     print("CNN, 2 Convolution Layer, 5 Epochs")
-    cnn1_model.fit(train_images, train_labels, epochs=5)
+    cnn1_model.fit(train_images, train_labels, epochs=5, validation_data=(test_images, test_labels))
     keras.saving.save_model(cnn1_model, "../models/cnn_conv2_gelu_5epoch")
 
     print("CNN, 2 Convolution Layer, 10 Epochs")
-    cnn1_model.fit(train_images, train_labels, epochs=5)
+    cnn1_model.fit(train_images, train_labels, epochs=5, validation_data=(test_images, test_labels))
     keras.saving.save_model(cnn1_model, "../models/cnn_conv2_gelu_10epoch")
