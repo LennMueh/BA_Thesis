c4c95051b8cb700823bdf1f571ae611786214c69
Lennart Mühlhahn <lennart.muehlhahn@tuhh.de>
2023-11-16 17:53:09 +0100

    Chnanged models

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import utilities.analysis as sa
 susp_num = 10
 star = 3
 
-model = dm.get_model("dnn_hid2_gelu_6epoch")
+model = dm.get_model("cnn_conv2_relu_6epoch")
 train_images, train_labels, test_images, test_labels = dm.get_data()
 correct_classifications, misclassifications, layer_outs, predictions = tn.test_model(model, test_images, test_labels)
 trainable_layers = tn.get_trainable_layers(model)
