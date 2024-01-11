7b1390a3c4b4143eafaaa7bebf2d677beb80c964
Lennart Mühlhahn <lennart.muehlhahn@tuhh.de>
2024-01-11 10:41:30 +0100

    11.01.2024

--- experiment_script.sh
+++ experiment_script.sh
@@ -2,11 +2,11 @@
 source ~/.virtualenvs/BA_Thesis/bin/activate
 #repeat 1 {python3 main.py "dnn2_1epoch_full"}
 #repeat 1 {python3 main.py "dnn2_1epoch_half"}
-#repeat 1 {python3 main.py "dnn2_1epoch_quarter"}
-#repeat 1 {python3 main.py "dnn2_6epoch_full"}
-#repeat 1 {python3 main.py "dnn2_6epoch_half"}
-#repeat 1 {python3 main.py "dnn2_6epoch_quarter"}
-#repeat 1 {python3 main.py "dnn3_1epoch_full"}
+repeat 1 {python3 main.py "dnn2_1epoch_quarter"}
+repeat 1 {python3 main.py "dnn2_6epoch_full"}
+repeat 1 {python3 main.py "dnn2_6epoch_half"}
+repeat 1 {python3 main.py "dnn2_6epoch_quarter"}
+repeat 1 {python3 main.py "dnn3_1epoch_full"}
 repeat 1 {python3 main.py "dnn3_1epoch_half"}
 repeat 1 {python3 main.py "dnn3_1epoch_quarter"}
 repeat 1 {python3 main.py "dnn3_6epoch_full"}
@@ -18,10 +18,10 @@ repeat 1 {python3 main.py "cnn1_1epoch_quarter"}
 repeat 1 {python3 main.py "cnn1_6epoch_full"}
 repeat 1 {python3 main.py "cnn1_6epoch_half"}
 repeat 1 {python3 main.py "cnn1_6epoch_quarter"}
-repeat 1 {python3 main.py "cnn2_1epoch_full"} 
-repeat 1 {python3 main.py "cnn2_1epoch_half"} 
+repeat 1 {python3 main.py "cnn2_1epoch_full"}
+repeat 1 {python3 main.py "cnn2_1epoch_half"}
 repeat 1 {python3 main.py "cnn2_1epoch_quarter"} 
 repeat 1 {python3 main.py "cnn2_6epoch_full"} 
 repeat 1 {python3 main.py "cnn2_6epoch_half"} 
 repeat 1 {python3 main.py "cnn2_6epoch_quarter"}
-# tarantula, random
+# tarantula, random, running: ociai
