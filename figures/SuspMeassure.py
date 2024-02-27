9d696bc5b8cc45f706705d220d79daf79317b233
Lennart Mühlhahn <lennart.muehlhahn@tuhh.de>
2024-02-27 18:41:25 +0100

    27.02.2024a

--- figures/SuspMeassure.py
+++ figures/SuspMeassure.py
@@ -11,7 +11,7 @@ plt.figure(figsize=(5, 5))
 labels_trained = [name for name, _ in df_trained]
 data_trained = [group['change_accuracy'].tolist() for name, group in df_trained]
 plt.boxplot(data_trained, labels=labels_trained, showfliers=False)
-plt.xlabel('Suspiciousness Meassure')
+plt.xlabel('Suspiciousness Measure')
 plt.ylabel('Change Accuracy')
 plt.xticks(rotation=45)  # Adjust rotation as needed
 plt.grid()
@@ -22,7 +22,7 @@ plt.figure(figsize=(5, 5))
 labels_not_trained = [name for name, _ in df_not_trained]
 data_not_trained = [group['change_accuracy'].tolist() for name, group in df_not_trained]
 plt.boxplot(data_not_trained, labels=labels_not_trained, showfliers=False)
-plt.xlabel('Suspiciousness Meassure')
+plt.xlabel('Suspiciousness Measure')
 plt.ylabel('Change Accuracy')
 plt.xticks(rotation=45)  # Adjust rotation as needed
 plt.grid()
@@ -33,7 +33,7 @@ plt.figure(figsize=(5, 5))
 labels_trained = [name for name, _ in df_trained]
 data_trained = [group['change_loss'].tolist() for name, group in df_trained]
 plt.boxplot(data_trained, labels=labels_trained, showfliers=False)
-plt.xlabel('Suspiciousness Meassure')
+plt.xlabel('Suspiciousness Measure')
 plt.ylabel('Change Loss')
 plt.xticks(rotation=45)  # Adjust rotation as needed
 plt.grid()
@@ -44,7 +44,7 @@ plt.figure(figsize=(5, 5))
 labels_not_trained = [name for name, _ in df_not_trained]
 data_not_trained = [group['change_loss'].tolist() for name, group in df_not_trained]
 plt.boxplot(data_not_trained, labels=labels_not_trained, showfliers=False)
-plt.xlabel('Suspiciousness Meassure')
+plt.xlabel('Suspiciousness Measure')
 plt.ylabel('Change Loss')
 plt.xticks(rotation=45)  # Adjust rotation as needed
 plt.grid()
